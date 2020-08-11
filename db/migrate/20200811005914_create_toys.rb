7af7d69c653ec0251fcd8a55ba5a2f288e4146b3
Kristofer <kristoferhenriquez@gmail.com>
2020-08-10 20:00:47 -0500

    initial commit

---
+++ db/migrate/20200811005914_create_toys.rb
@@ -0,0 +1,11 @@
+class CreateToys < ActiveRecord::Migration[6.0]
+  def change
+    create_table :toys do |t|
+      t.string :name
+      t.integer :size
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
